ebcf02feb6d1d134006110c4c0d4a836e9b534b7
jantonio2 <jesusrochavedia@gmail.com>
2022-08-30 18:40:12 -0400

    Parte del título diseñado

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -6,11 +6,43 @@ class BasicDesignScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape1.jpg')),
+    return Scaffold(
+      body: Column(
+        children: const[
+          Image(image: AssetImage('assets/landscape1.jpg')),
+          Title()
+        ],
       ),
     );
   }
 
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('Más allá del muro', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+              SizedBox(height: 6),
+              Text('Winterfell, Westeros', style: TextStyle(color: Colors.black45))
+            ],
+          ),
+          Expanded(child: Container()),
+          const Icon(Icons.star, color: Colors.red),
+          const SizedBox(width: 5),
+          const Text('41')
+        ],
+      ),
+    );
+  }
 }
