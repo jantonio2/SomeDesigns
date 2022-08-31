c725f856dac814e31dccf38f2b234898a74bb38c
jantonio2 <jesusrochavedia@gmail.com>
2022-08-31 14:24:45 -0400

    Terminando el primer screen del segundo diseño

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:designs/screens/screens.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, // transparent status bar
+  ));
+  runApp(const MyApp());
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- lib/screens/scroll_design.dart
+++ lib/screens/scroll_design.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ScrollDesignScrenn extends StatelessWidget {
@@ -25,15 +27,35 @@ class MainContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text('Hola'),
-        Text('Hola'),
-        Text('Hola'),
-        Text('Hola'),
-      ],
+
+    const TextStyle textStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white);
+
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 50),
+          const Text('11°', style: textStyle),
+          const Text('Miércoles', style: textStyle),
+          Expanded(child: Container()),
+          // const Icon(Icons.keyboard_arrow_down, size: 100, color: Colors.white),
+          Transform.rotate(
+            angle: 3.14 / 4,
+            child: Container(
+              margin: const EdgeInsets.only(right: 30.0, bottom: 30.0),
+              height: 50.0,
+              width: 50.0,
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.white, width: 4.0),
+                  right: BorderSide(color: Colors.white, width: 4.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -46,7 +68,8 @@ class Background extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xff30BAD6),
+      // color: const Color(0xff30BAD6),
+      color: const Color.fromRGBO(80, 194, 221, 1.0),
       height: double.infinity,
       alignment: Alignment.topCenter,
       child: const Image(image: AssetImage('assets/scroll-1.png'))
