e688fa908edc7b15c96ab073090ee84e1e2ebe0c
jantonio2 <jesusrochavedia@gmail.com>
2022-08-30 20:44:05 -0400

    Primera pantalla terminada

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -8,9 +8,14 @@ class BasicDesignScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const[
-          Image(image: AssetImage('assets/landscape1.jpg')),
-          Title()
+        children: [
+          const Image(image: AssetImage('assets/landscape1.jpg')),
+          const Title(),
+          const ButtonSection(),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+            child: const Text('Duis laborum velit adipisicing quis. Est et cillum esse amet dolor proident amet aute mollit consequat. Est anim laborum nisi dolor do excepteur occaecat nulla elit est deserunt. Non laborum cillum minim duis mollit est sint anim eu. Quis ipsum adipisicing voluptate deserunt aliqua velit amet commodo anim eu irure aute laborum laborum. Amet cillum veniam voluptate qui Lorem.')
+          )
         ],
       ),
     );
@@ -26,7 +31,7 @@ class Title extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
       child: Row(
         children: [
           Column(
@@ -43,6 +48,59 @@ class Title extends StatelessWidget {
           const Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: const[
+          ButtonSectionIcon(
+            icono: Icons.call,
+            texto: 'CALL',
+          ),
+          ButtonSectionIcon(
+            icono: Icons.near_me,
+            texto: 'ROUTE',
+          ),
+          ButtonSectionIcon(
+            icono: Icons.share,
+            texto: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSectionIcon extends StatelessWidget {
+
+  final IconData icono;
+  final String texto;
+
+  const ButtonSectionIcon({
+    Key? key,
+    required this.icono,
+    required this.texto,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icono, color: Colors.blue, size: 30),
+        const SizedBox(height: 10),
+        Text(texto, style: const TextStyle(color: Colors.blue, fontSize: 16))
+      ],
     );
   }
 }
