6bd513e255e80399670a4122eb7a522831987671
jantonio2 <jesusrochavedia@gmail.com>
2022-09-04 00:28:23 -0400

    Aplicando blur a las opciones del HomeBody

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
+      theme: ThemeData.dark(),
       initialRoute: 'home_screen',
       routes: {
         'basic_design': (_) => const BasicDesignScreen(),
--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
@@ -47,22 +49,30 @@ class _SingleCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
-      height: 180,
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(62, 66, 107, 0.7),
-        borderRadius: BorderRadius.circular(20)
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundColor: color,
-            radius: 30,
-            child: Icon(icon, size: 35),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            height: 180,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: color,
+                  radius: 30,
+                  child: Icon(icon, size: 35, color: Colors.white),
+                ),
+                const SizedBox(height: 30),
+                Text(text, style: TextStyle(color: color, fontSize: 18))
+              ],
+            ),
           ),
-          const SizedBox(height: 30),
-          Text(text, style: TextStyle(color: color, fontSize: 18))
-        ],
+        ),
       ),
     );
   }
